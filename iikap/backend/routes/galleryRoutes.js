1e7c5caac69139e60a063d2643231ce89c961950
Blake796 <darrelkipkoech674@gmail.com>
2026-02-24 10:56:38 +0300

    Delete iikap directory

--- iikap/backend/routes/galleryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const galleryController = require('../controllers/galleryController');
-
-router.get('/', galleryController.getGalleryImages);
-
-module.exports = router;
